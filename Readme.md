81cb3037e541eb6911a1f2a1b5eea015f2623ec3
Ólafur Helgason <olafur@oz.com>
2013-06-27 15:45:42 +0000

    Added Readme and install.sh

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## Dotvim
+
+##### Installation
+
+From your homedirectory (on Linux/Mac OSX):
+
+* `git clone git://github.com/olafur/dotvim.git`
+* `ln -sfn dotvim .vim`
+* `ln -sfn dotvim/vimrc .vimrc`
+* `git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle`
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+create_symlinks () {
+    if [ ! -f ~/.vim ]; then
+        ln -sfn ~/dotvim ~/.vim
+    fi
+
+    if [ ! -f ~/.vimrc ]; then
+        ln -sfn ~/dotvim/vimrc ~/.vimrc
+    fi
+}
+
+create_symlinks
+
+if [ ! -d "bundle" ]; then
+    echo "Creating bundle directory"
+    mkdir bundle
+fi
+
+if [ ! -d "bundle/vundle" ]; then
+    echo "Installing vundle"
+    git clone https://github.com/gmarik/vundle.git bundle/vundle
+fi
+
+
+vim +BundleInstall +qall
+
